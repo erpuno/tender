ac0b1d1994afe82009a39fa8079ca9da64219cf7
5HT <maxim@synrc.com>
2022-12-06 11:39:38 +0200

    config.exs

--- config/config.exs
+++ config/config.exs
@@ -6,7 +6,7 @@ config :n2o,
     %{ level: :info, id: :synrc, max_size: 2000, module: :logger_std_h, config: %{type: :file, file: 'smarttender.log'},
        formatter: {:logger_formatter, %{ template: [:time, ' ', :pid, ' ', :module, ' ', :msg, '\n'], single_line: true }}}}],
   tender_upload: 'https://api-test.smarttender.biz/prozorro/',
-  tender_bearer: 'Basic ',
+  tender_bearer: 'Basic 1',
   jwt_prod: false,
   login: "Максим Сохацький",
   logger_level: :info
